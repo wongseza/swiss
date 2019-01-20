edf86f95248d44d1f5949c2de83ff1f8cffb57af
wongseza <wongsagorn1@hotmail.com>
2019-01-20 15:10:21 +0700

    Add result table

--- script.js
+++ script.js
@@ -1,63 +1,153 @@
+// Counter
 var fromCount = 0;
 var toCount = 0;
+var connectionsCount = 0;
+
+// Input parameter
 var from = document.querySelector('#from');
 var to = document.querySelector('#to');
-var fromOptionList = document.querySelectorAll('td.fromOption');
-var toOptionList = document.querySelectorAll('td.toOption');
 var date = document.querySelector('#date');
 var time = document.querySelector('#time');
+var isArrivalTime = document.querySelector('#isArrivalTime');
+var train = document.querySelector('#train');
+var tram = document.querySelector('#tram');
+var ship = document.querySelector('#ship');
+var bus = document.querySelector('#bus');
+var cableway = document.querySelector('#cableway');
+
+// Others
+var optionFromList = document.querySelectorAll('td.optionFrom');
+var optionToList = document.querySelectorAll('td.optionTo');
 var parameter = document.querySelector('table.parameter');
+var result = document.querySelector('table.result');
 var search = document.querySelector('button.search');
 var iconList = ["sl-icon-type-bus", "sl-icon-type-fun", "sl-icon-type-sb", "sl-icon-type-ship", "sl-icon-type-tram", "sl-icon-type-zug", "sl-icon-type-gondola", "sl-icon-type-chairlift", "sl-icon-type-train", "sl-icon-type-post", "sl-icon-type-night-bus", "sl-icon-type-strain", "sl-icon-type-night-strain", "sl-icon-type-express-train"];
+var searchAlready = false;
 
 var currentTime = new Date();
 date.valueAsDate = currentTime;
-hours = currentTime.getHours();
-minutes = currentTime.getMinutes();
-hoursString = (hours > 9) ? String(hours) : "0" + hours;
-minutesString = (minutes > 9) ? String(minutes) : "0" + minutes;
-time.value = hoursString + ":" + minutesString;
+time.value = currentTime.toTimeString().substring(0, 5);
 
 from.addEventListener('input', function() {
   fromCount++;
-  httpGet('https://timetable.search.ch/api/completion.json?term=' + from.value, 'from', locationsCallback, fromCount);
+  httpGet('https://timetable.search.ch/api/completion.json?term=' + from.value, locationsCallback, fromCount, 'from');
 });
 to.addEventListener('input', function() {
   toCount++;
-  httpGet('https://timetable.search.ch/api/completion.json?term=' + to.value, 'to', locationsCallback, toCount);
+  httpGet('https://timetable.search.ch/api/completion.json?term=' + to.value, locationsCallback, toCount, 'to');
 });
 
-fromOptionList.forEach(function(fromOption) {
-  fromOption.addEventListener('click', function() {
-    if (fromOption.innerHTML === "") {
+optionFromList.forEach(function(optionFrom) {
+  optionFrom.addEventListener('click', function() {
+    if (optionFrom.innerHTML === "") {
       return;
     }
-    from.value = fromOption.value;
+    from.value = optionFrom.value;
   });
 });
-toOptionList.forEach(function(toOption) {
-  toOption.addEventListener('click', function() {
-    if (toOption.innerHTML === "") {
+optionToList.forEach(function(optionTo) {
+  optionTo.addEventListener('click', function() {
+    if (optionTo.innerHTML === "") {
       return;
     }
-    to.value = toOption.value;
+    to.value = optionTo.value;
   });
+});
+
+search.addEventListener('click', function() {
+  var fromValue = from.value;
+  var toValue = to.value;
+  var dateValue = date.value;
+  var timeValue = time.value;
+  var isArrivalTimeValue = isArrivalTime.checked;
+  var trainValue = train.checked;
+  var tramValue = tram.checked;
+  var shipValue = ship.checked;
+  var busValue = bus.checked;
+  var cablewayValue = cableway.checked;
+  
+  if (!fromValue) {
+    alert("Please specify from.");
+    return;
+  }
+  if (!toValue) {
+    alert("Please specify to.");
+    return;
+  }
+  if (!trainValue && !tramValue && !shipValue && !busValue && !cablewayValue) {
+    alert("Please select at least one transportation mode.");
+    return;
+  }
+  
+  var url = "http://transport.opendata.ch/v1/connections?limit=5";
+  url += "&from=" + fromValue;
+  url += "&to=" + toValue;
+  url += "&date=" + dateValue;
+  url += "&time=" + timeValue;
+  if (isArrivalTimeValue) {
+    url += "&isArrivalTime=1";
+  }
+  if (!trainValue || !tramValue || !shipValue || !busValue || !cablewayValue) {
+    if (trainValue) {
+      url += "&transportations[]=train";
+    }
+    if (tramValue) {
+      url += "&transportations[]=tram";
+    }
+    if (shipValue) {
+      url += "&transportations[]=ship";
+    }
+    if (busValue) {
+      url += "&transportations[]=bus";
+    }
+    if (cablewayValue) {
+      url += "&transportations[]=cableway";
+    }
+  }
+  httpGet(url, connectionsCallback, connectionsCount);
 });
 
 document.addEventListener('click', function() {
-  hideOptions(fromOptionList);
-  hideOptions(toOptionList);
+  hideOptions(optionFromList);
+  hideOptions(optionToList);
   parameter.style.display = "table";
   search.style.display = "block";
+  if (searchAlready) {
+    result.style.display = "table";
+  }
 });
 
-function hideOptions(optionList) {
-  for (option of optionList) {
-    option.value = "";
-    option.innerHTML = "";
-    option.style.backgroundColor = "white";
-    option.style.padding = "0pt";
+function connectionsCallback(json, type, callCount) {
+  var resultFromList      = document.querySelectorAll('td.resultFrom');
+  var resultToList        = document.querySelectorAll('td.resultTo');
+  var resultDepartureList = document.querySelectorAll('td.resultDeparture');
+  var resultArrivalList   = document.querySelectorAll('td.resultArrival');
+  var resultDurationList  = document.querySelectorAll('td.resultDuration');
+  var resultTransfersList  = document.querySelectorAll('td.resultTransfers');
+  var resultProductsList  = document.querySelectorAll('td.resultProducts');
+  
+  for (i = 0; i < resultFromList.length; i++) {
+    resultFromList[i].innerHTML = "";
+    resultToList[i].innerHTML = "";
+    resultDepartureList[i].innerHTML = "";
+    resultArrivalList[i].innerHTML = "";
+    resultDurationList[i].innerHTML = "";
+    resultTransfersList[i].innerHTML = "";
+    resultProductsList[i].innerHTML = "";
   }
+  
+  var connections = json.connections;
+  for (i = 0; i < connections.length; i++) {
+    resultFromList[i].innerHTML = connections[i].from.station.name;
+    resultToList[i].innerHTML = connections[i].to.station.name;
+    resultDepartureList[i].innerHTML = connections[i].from.departure.substring(11, 16);
+    resultArrivalList[i].innerHTML = connections[i].to.arrival.substring(11, 16);
+    resultDurationList[i].innerHTML = connections[i].duration.substring(3, 8);
+    resultTransfersList[i].innerHTML = connections[i].transfers;
+    resultProductsList[i].innerHTML = connections[i].products.join(", ");
+  }
+  result.style.display = "table";
+  searchAlready = true;
 }
 
 function locationsCallback(json, type, callCount) {
@@ -71,38 +161,47 @@ function locationsCallback(json, type, callCount) {
     if (fromCount != callCount) {
       return;
     }
-    for (fromOption of fromOptionList) {
-      fromOption.value = "";
-      fromOption.innerHTML = "";
-      fromOption.style.backgroundColor = "white";
-      fromOption.style.padding = "0pt";
+    for (optionFrom of optionFromList) {
+      optionFrom.value = "";
+      optionFrom.innerHTML = "";
+      optionFrom.style.backgroundColor = "white";
+      optionFrom.style.padding = "0pt";
     }
     for (var i = 0; i < stationList.length; i++) {
-      fromOptionList[i].value = stationList[i].label;
-      fromOptionList[i].innerHTML = stationList[i].html ? stationList[i].html : stationList[i].label;
-      fromOptionList[i].style.padding = "5pt 5pt 5pt 20pt";
+      optionFromList[i].value = stationList[i].label;
+      optionFromList[i].innerHTML = stationList[i].html ? stationList[i].html : stationList[i].label;
+      optionFromList[i].style.padding = "5pt 5pt 5pt 20pt";
     }
   } else {
     if (toCount != callCount) {
       return;
     }
-    for (toOption of toOptionList) {
-      toOption.value = "";
-      toOption.innerHTML = "";
-      toOption.style.backgroundColor = "white";
-      toOption.style.padding = "0pt";
+    for (optionTo of optionToList) {
+      optionTo.value = "";
+      optionTo.innerHTML = "";
+      optionTo.style.backgroundColor = "white";
+      optionTo.style.padding = "0pt";
     }
     for (var i = 0; i < stationList.length; i++) {
-      toOptionList[i].value = stationList[i].label;
-      toOptionList[i].innerHTML = stationList[i].html ? stationList[i].html : stationList[i].label;
-      toOptionList[i].style.padding = "5pt 5pt 5pt 20pt";
+      optionToList[i].value = stationList[i].label;
+      optionToList[i].innerHTML = stationList[i].html ? stationList[i].html : stationList[i].label;
+      optionToList[i].style.padding = "5pt 5pt 5pt 20pt";
     }
   }
-  parameter.style.display = "none";
-  search.style.display = "none";
+  if (stationList.length > 0) {
+    parameter.style.display = "none";
+    search.style.display = "none";
+    result.style.display = "none";
+  } else {
+    parameter.style.display = "table";
+    search.style.display = "block";
+    if (searchAlready) {
+      result.style.display = "table";
+    }
+  }
 }
 
-function httpGet(url, type, callback, callCount) {
+function httpGet(url, callback, callCount, type='') {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200) {
@@ -111,4 +210,13 @@ function httpGet(url, type, callback, callCount) {
   }
   xmlHttp.open("GET", url, true);
   xmlHttp.send(null);
+}
+
+function hideOptions(optionList) {
+  for (option of optionList) {
+    option.value = "";
+    option.innerHTML = "";
+    option.style.backgroundColor = "white";
+    option.style.padding = "0pt";
+  }
 }
